c4180f7f9092d78b51404851e25a42e931090a32
Victoria <vigi2586@stud.kea.dk>
2021-02-11 16:33:19 +0100

    getting started

--- bar_chart.js
+++ bar_chart.js
@@ -9,20 +9,6 @@ const visits = getNumber();
 
 function start() {
 
-  //add item in the beginning with unshift
-  array.unshift(numbers);
-
-  numbers++;
-
-  //can not be longer than 9
-  if (numbers > 9) {
-    array.pop(array);
-  }
-
-  setTimeout(start, 1000);
-
-  console.log(array);
-
 };
 
 function getNumber () {
