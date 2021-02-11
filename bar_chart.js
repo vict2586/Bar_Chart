e173ee73282c3e361818177b7c2669f17c0087dd
Victoria <vigi2586@stud.kea.dk>
2021-02-11 16:32:50 +0100

    getting started

--- bar_chart.js
+++ bar_chart.js
@@ -1,3 +1,32 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", start);
+
+
+const model = []
+
+const visits = getNumber();
+
+function start() {
+
+  //add item in the beginning with unshift
+  array.unshift(numbers);
+
+  numbers++;
+
+  //can not be longer than 9
+  if (numbers > 9) {
+    array.pop(array);
+  }
+
+  setTimeout(start, 1000);
+
+  console.log(array);
+
+};
+
+function getNumber () {
+
+  return Math.floor(Math.random()*32);
+
+}
